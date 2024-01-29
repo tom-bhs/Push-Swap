d4b876108bd7036135f779c05314cf71178b7ea7
Tom Bihoues <tbihoues@c1r4p3.42lehavre.fr>
2024-01-29 18:09:19 +0100

    add

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -5,8 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbihoues <tbihoues@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/29 17:12:02 by tbihoues          #+#    #+#             */
-/*   Updated: 2024/01/29 17:12:03 by tbihoues         ###   ########.fr       */
+/*   Created: 2024/01/29 17:35:42 by tbihoues          #+#    #+#             */
+/*   Updated: 2024/01/29 17:37:44 by tbihoues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft.h"
+#include <unistd.h>
